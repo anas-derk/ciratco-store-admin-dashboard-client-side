5e64e664c0b0516641a79efc7609327d2e8f0136
anas derk <anas.derk@gmail.com>
2026-02-04 03:34:35 +0300

    Fix

--- src/pages/products-managment/add-new-product/index.jsx
+++ src/pages/products-managment/add-new-product/index.jsx
@@ -341,7 +341,7 @@ export default function AddNewProduct() {
     const handleSearchOfCountry = (e) => {
         const searchedCountry = e.target.value;
         if (searchedCountry) {
-            setFilteredCountryList(filteredCountryList.filter((country) => countries[country].name.toLowerCase().startsWith(searchedCountry.toLowerCase())));
+            setFilteredCountryList(countryList.filter((country) => countries[country].name.toLowerCase().startsWith(searchedCountry.toLowerCase())));
         } else {
             setFilteredCountryList(countryList);
         }
